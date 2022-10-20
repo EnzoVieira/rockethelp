b5f0f94bd1231370655af4d4df3c55edaee6b046
Enzo Vieira <enzovieira16@hotmail.com>
2022-10-20 18:58:06 +0100

    add: Register new order on Firebase

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,7 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { Alert } from "react-native"
+import auth from "@react-native-firebase/auth"
+import firestore from "@react-native-firebase/firestore"
 import { useNavigation } from "@react-navigation/native"
 import {
   VStack,
@@ -12,24 +15,21 @@ import {
 } from "native-base"
 import { SignOut, ChatTeardropText } from "phosphor-react-native"
 
+import { dateFormat } from "../utils/firestoreDateFormat"
+
 import Logo from "../assets/logo_secondary.svg"
 
 import { Filter } from "../components/Filter"
 import { Order, IOrderProps } from "../components/Order"
 import { Button } from "../components/Button"
+import { Loading } from "../components/Loading"
 
 export const Home = () => {
+  const [isLoading, setIsLoading] = useState(false)
   const [statusSelected, setStatusSelected] = useState<"open" | "closed">(
     "open"
   )
-  const [orders, setOrders] = useState<IOrderProps[]>([
-    {
-      id: "123",
-      patrimony: "29743",
-      when: "18/07/2022 às 10:00",
-      status: "open",
-    },
-  ])
+  const [orders, setOrders] = useState<IOrderProps[]>([])
 
   const navigation = useNavigation()
   const { colors } = useTheme()
@@ -41,6 +41,41 @@ export const Home = () => {
   const handleOpenDetails = (orderId: string) => {
     navigation.navigate("details", { orderId })
   }
+
+  const handleLogout = () => {
+    auth()
+      .signOut()
+      .catch((error) => {
+        console.log(error)
+        return Alert.alert("Sair", "Não foi possível sair.")
+      })
+  }
+
+  useEffect(() => {
+    setIsLoading(true)
+
+    const subscriber = firestore()
+      .collection("orders")
+      .where("status", "==", statusSelected)
+      .onSnapshot((snapshot) => {
+        const data = snapshot.docs.map((doc) => {
+          const { patrimony, description, status, created_at } = doc.data()
+
+          return {
+            id: doc.id,
+            patrimony,
+            description,
+            status,
+            when: dateFormat(created_at),
+          }
+        })
+
+        setOrders(data)
+        setIsLoading(false)
+      })
+
+    return subscriber
+  }, [statusSelected])
 
   return (
     <VStack flex={1} pb={6} bg="gray.700">
@@ -55,7 +90,10 @@ export const Home = () => {
       >
         <Logo />
 
-        <IconButton icon={<SignOut size={26} color={colors.gray[300]} />} />
+        <IconButton
+          icon={<SignOut size={26} color={colors.gray[300]} />}
+          onPress={handleLogout}
+        />
       </HStack>
 
       <VStack flex={1} px={6}>
@@ -88,25 +126,29 @@ export const Home = () => {
           />
         </HStack>
 
-        <FlatList
-          data={orders}
-          keyExtractor={({ id }) => id}
-          renderItem={({ item }) => (
-            <Order data={item} onPress={() => handleOpenDetails(item.id)} />
-          )}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{ paddingBottom: 100 }}
-          ListEmptyComponent={() => (
-            <Center>
-              <ChatTeardropText size={40} color={colors.gray[300]} />
-              <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
-                Você ainda não possui {"\n"}
-                solicitações{" "}
-                {statusSelected === "open" ? "em andamento" : "finalizadas"}
-              </Text>
-            </Center>
-          )}
-        />
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <FlatList
+            data={orders}
+            keyExtractor={({ id }) => id}
+            renderItem={({ item }) => (
+              <Order data={item} onPress={() => handleOpenDetails(item.id)} />
+            )}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 100 }}
+            ListEmptyComponent={() => (
+              <Center>
+                <ChatTeardropText size={40} color={colors.gray[300]} />
+                <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                  Você ainda não possui {"\n"}
+                  solicitações{" "}
+                  {statusSelected === "open" ? "em andamento" : "finalizadas"}
+                </Text>
+              </Center>
+            )}
+          />
+        )}
 
         <Button title="Nova solicitação" onPress={handleNewOrder} />
       </VStack>
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import { Alert } from "react-native"
+import auth from "@react-native-firebase/auth"
 import { Heading, VStack, Icon, useTheme } from "native-base"
 import { Envelope, Key } from "phosphor-react-native"
 
@@ -7,13 +9,42 @@ import { Button } from "../components/Button"
 import { Input } from "../components/Input"
 
 export const SignIn = () => {
-  const [name, setName] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+  const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
   const { colors } = useTheme()
 
   const handleSignIn = () => {
-    console.log(name, password)
+    if (!email || !password) {
+      return Alert.alert("Entrar", "Informa e-mail e senha.")
+    }
+
+    setIsLoading(true)
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+
+        if (error.code === "auth/invalid-email") {
+          return Alert.alert("Entrar", "E-mail inválido.")
+        }
+
+        if (error.code === "auth/wrong-password") {
+          return Alert.alert("Entrar", "E-mail ou senha inválida.")
+        }
+
+        if (error.code === "auth/user-not-found") {
+          return Alert.alert("Entrar", "E-mail ou senha inválida.")
+        }
+
+        return Alert.alert("Entrar", "Não foi possível acessar.")
+      })
+      .finally(() => setIsLoading(false))
   }
 
   return (
@@ -36,7 +67,8 @@ export const SignIn = () => {
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
-        onChangeText={setName}
+        onChangeText={setEmail}
+        autoCapitalize="none"
       />
 
       <Input
@@ -47,7 +79,12 @@ export const SignIn = () => {
         onChangeText={setPassword}
       />
 
-      <Button title="Entrar" w="full" onPress={handleSignIn} />
+      <Button
+        title="Entrar"
+        w="full"
+        onPress={handleSignIn}
+        isLoading={isLoading}
+      />
     </VStack>
   )
 }
